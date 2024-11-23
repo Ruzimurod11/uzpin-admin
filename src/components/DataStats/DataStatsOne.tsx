4982b284c7ced37ff0273f98a61beb02935a833d
jamshidqayimov <jamshidqayimov0399@gmail.com>
2024-11-23 07:22:15 +0500

    fixed

--- src/components/DataStats/DataStatsOne.tsx
+++ src/components/DataStats/DataStatsOne.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { dataStats } from "@/types/dataStats";
 
-const DataStatsOne: React.FC<dataStats> = () => {
+const DataStatsOne = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
