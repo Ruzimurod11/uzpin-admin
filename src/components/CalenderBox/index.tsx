72b84101b122c327a525970d94755612f853e4a2
jamshidqayimov <jamshidqayimov0399@gmail.com>
2024-11-23 07:25:53 +0500

    edit css

--- src/components/CalenderBox/index.tsx
+++ src/components/CalenderBox/index.tsx
@@ -1,7 +1,7 @@
 const CalendarBox = () => {
   return (
     <>
-      <div className="w-full max-w-full rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
+      <div className="">
         BU YER HORIZCHA BOSH AMMO HOZIRCHA 😉
       </div>
     </>
