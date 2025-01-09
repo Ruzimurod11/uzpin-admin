2237680cd4b219efd3f1e89aa6e7ddf57eadd70a
Omonov <algoritm1202@gmail.com>
2025-01-09 16:58:34 +0500

    border none

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 const SignIn: React.FC = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
-      <div className="m-auto w-1/2 border">
+      <div className="m-auto w-1/2">
         <div className="flex flex-wrap items-center">
           <div className="w-full">
             <div className="w-full p-4 sm:p-12.5 xl:p-15">
